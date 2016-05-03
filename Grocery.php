a94349778b65ab2c7edb411cb1ee02c37a544612
ib0346a <ib0346a@student.american.edu>
2016-05-03 01:32:04 -0400

    Added files via upload

---
+++ Grocery.php
@@ -0,0 +1,22 @@
+	<?php 	
+		//Calls Common.php
+		include "Common.php";
+	?>
+	<!--Calls Header from Common.php-->
+	<?=$head;?>
+
+	<!--Calls Navigation Bar from Common.php-->
+	<?=$NavigationBar;?>
+	<?=$GroceryNavBar;?>
+
+		
+		<div class="right_div" id="Shopping">
+			<div class="innerproductsdiv">
+			<img id="grocery_page_img" src="images/gbackgound.jpg">
+			</div>
+			</div>
+		</div>
+	</div>
+
+  	  	<!--Calls html closer from Common.php-->
+		<?=$htmlcloser?>
---
+++ HomePage.php
@@ -0,0 +1,18 @@
+	<?php 	
+		//Calls Common.php
+		include "Common.php";
+	?>
+	<!--Calls Header from Common.php-->
+	<?=$head;?>
+
+	<!--Calls Navigation Bar from Common.php-->
+	<?=$NavigationBar;?>
+	<br>
+	<div class="body_div_main">
+		<center><img id="main_page_img" src="images/mainpageimg.jpg" alt="mainimg"><center>
+		
+		
+    </div>
+		
+    <!--Calls Footer from Common.php-->
+	<?=$footer;?>
